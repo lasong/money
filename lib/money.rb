0d7ca59d8e8d0fea97a23276149d40a71314ed09
Lekealem Asong <Lekealem.Asong@invision.de>
2015-11-05 21:21:21 +0100

    Refactor code

--- lib/money.rb
+++ lib/money.rb
@@ -33,57 +33,51 @@ class Money
       Error,
       'Configure conversion rates') unless currency_convertable?(other_currency)
 
-    Money.new(amount * amount_conversion_rate(other_currency), other_currency)
+    Money.new(amount * conversion_rate_for(other_currency), other_currency)
   end
 
   def +(other)
-    Money.new(amount + other_converted(other).amount, currency)
+    Money.new(amount + other.convert_to(currency).amount, currency)
   end
 
   def -(other)
-    Money.new(amount - other_converted(other).amount, currency)
+    Money.new(amount - other.convert_to(currency).amount, currency)
   end
 
   def /(number)
-    Money.new(@amount / number.to_f, currency)
+    Money.new(amount / number.to_f, currency)
   end
 
   def *(number)
-    Money.new(@amount * number.to_f, currency)
+    Money.new(amount * number, currency)
   end
 
   def ==(other)
-    other_converted_object = other_converted(other)
-    (-0.01..0.01).include?((amount - other_converted_object.amount).round(2))
+    converted_other = other.convert_to(currency)
+    (-0.01..0.01).include?((amount - converted_other.amount).round(2))
   end
 
   def >(other)
-    (amount - other_converted(other).amount).round(2) > 0.01
+    (amount - other.convert_to(currency).amount).round(2) > 0.01
   end
 
   def <(other)
-    (amount - other_converted(other).amount).round(2) < -0.01
+    (amount - other.convert_to(currency).amount).round(2) < -0.01
   end
 
   private
-
-  def other_converted(other_object)
-    return other_object if currency == other_object.currency
-
-    other_object.clone.convert_to(currency)
-  end
 
   def currency_convertable?(other_currency)
     !Money.rates_by_currency[currency].empty? ||
       !Money.rates_by_currency[other_currency].empty?
   end
 
-  def amount_conversion_rate(other_currency)
+  def conversion_rate_for(other_currency)
     rate = Money.rates_by_currency[currency][other_currency]
-    return rate if rate
+    return rate.to_f if rate
 
     rate = Money.rates_by_currency[other_currency][currency]
-    return 1.0 / rate if rate
+    return 1 / rate.to_f if rate
 
     fail(
       Error, "Conversion rates for #{other_currency} does not exist")
